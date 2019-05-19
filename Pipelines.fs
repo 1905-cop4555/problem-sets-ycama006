8c4bc1aef9eadae1636f6c2b4bb3b482f49c21d7
ycama006 <32624262+ycama006@users.noreply.github.com>
2019-05-19 08:03:16 -0400

    Attempted examples of pipelines in F#

---
+++ Pipelines.fs
@@ -0,0 +1,114 @@
+﻿(*
+
+OBJECTIVE: Practicing pipelines and composition with F#
+
+*)
+
+
+
+
+module PipelinesAndComposition =
+
+
+
+    /// Squares a value.
+    let square x = x * x
+
+    /// Adds 1 to a value.
+    let addOne x = x + 1
+
+    /// Tests if an integer value is odd via modulo.
+    let isOdd x = x % 2 <> 0
+    printfn "isOdd: %b" (isOdd 9)
+    printfn "isOdd: %b\n\n" (isOdd 8)
+
+    /// A list of 5 numbers.  More on lists later.
+    let numbers = [ 1; 2; 3; 4; 5 ]
+
+    /// Given a list of integers, it filters out the even numbers,
+    /// squares the resulting odds, and adds 1 to the squared odds.
+    let squareOddValuesAndAddOne values = 
+        let odds = List.filter isOdd values
+        let squares = List.map square odds
+        let result = List.map addOne squares
+        result
+
+
+
+
+
+
+    printfn "processing %A through 'squareOddValuesAndAddOne' produces: %A" numbers (squareOddValuesAndAddOne numbers)
+
+
+
+    
+    /// A shorter way to write 'squareOddValuesAndAddOne' is to nest each
+    /// sub-result into the function calls themselves.
+    ///
+    /// This makes the function much shorter, but it's difficult to see the
+    /// order in which the data is processed.
+    let squareOddValuesAndAddOneNested values = 
+        List.map addOne ( List.map square (List.filter isOdd values) )
+
+    printfn "processing %A through 'squareOddValuesAndAddOneNested' produces: %A" numbers (squareOddValuesAndAddOneNested numbers)
+
+
+
+
+    /// A preferred way to write 'squareOddValuesAndAddOne' is to use F# pipe operators.
+    /// This allows you to avoid creating intermediate results, but is much more readable
+    /// than nesting function calls like 'squareOddValuesAndAddOneNested'
+    let squareOddValuesAndAddOnePipeline values =
+        values
+        |> List.filter isOdd
+        |> List.map square
+        |> List.map addOne
+
+    printfn "processing %A through 'squareOddValuesAndAddOnePipeline' produces: %A" numbers (squareOddValuesAndAddOnePipeline numbers)
+
+
+
+
+
+    /// You can shorten 'squareOddValuesAndAddOnePipeline' by moving the second `List.map` call
+    /// into the first, using a Lambda Function.
+    ///
+    /// Note that pipelines are also being used inside the lambda function.  F# pipe operators
+    /// can be used for single values as well.  This makes them very powerful for processing data.
+    let squareOddValuesAndAddOneShorterPipeline values =
+        values
+        |> List.filter isOdd
+        |> List.map(fun x -> x |> square |> addOne)
+
+    printfn "processing %A through 'squareOddValuesAndAddOneShorterPipeline' produces: %A" numbers (squareOddValuesAndAddOneShorterPipeline numbers)
+
+
+
+
+
+
+
+
+
+(*
+
+
+    Pipelines and Composition
+
+Pipe operators such as |> are used extensively when processing data in F#. 
+These operators are functions that allow you to establish "pipelines" of 
+functions in a flexible manner. 
+
+The previous sample made use of many features of F#, including list 
+processing functions, first-class functions, and partial application. 
+Although a deep understanding of each of those concepts can become somewhat 
+advanced, it should be clear how easily functions can be used to process data 
+when building pipelines.
+
+
+*)
+
+
+
+//REFERENCE: https://docs.microsoft.com/en-us/dotnet/fsharp/tour
