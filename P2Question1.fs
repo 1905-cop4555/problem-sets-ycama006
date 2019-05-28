a8b903a4a9c9c5fd4da2333d0a91550df2b35802
ycama006 <32624262+ycama006@users.noreply.github.com>
2019-05-28 19:55:13 -0400

    Converting curried function to uncurried function; vice versa
    Defined a F# function curry f that converts an uncurried function to a curried function, and an F# function uncurry f that does the opposite conversion.

---
+++ P2Question1.fs
@@ -0,0 +1,45 @@
+(*
+
+
+OBJECTIVE: Define a F# function curry f that converts an uncurried function to a curried function, 
+           and an F# function uncurry f that does the opposite conversion.
+
+*)
+
+
+
+module P2Question1 = 
+
+
+
+
+   //curry f = converts an uncurried function to a curried function
+
+
+   //uncurry f = converts a curry function to a uncurried function
+
+
+   ///-----------------------------------------------------------------------
+
+
+    let addp (x,y) = x + y
+    printfn "\n\n(uncurry)addp function outputs: %d" ( addp (2,3) )
+
+    let addc x y = x + y
+    printfn "(curry)addc function outputs: %d" ( addc 3 10 )
+
+
+    ///---------------------------------------------------------------------
+
+
+    let uncurry f (a,b) = f a b
+    let uncurriedPlus = uncurry (+) 
+    printfn "\n\n(uncurry)plus function outputs: %d" ( uncurriedPlus (2,3) )
+
+
+
+    let curry f a b = f (a,b)
+    let curriedPlus = curry uncurriedPlus
+    let plus3 = curriedPlus 3
+    printfn "(curry)plus function outputs: %d" ( plus3 10 )
+    
