15b7cf917d6b80a3864c69983e0507a4e0af69d9
ycama006 <32624262+ycama006@users.noreply.github.com>
2019-06-10 23:59:53 -0400

    Problem Set 3, Question 6
    Was able to recreate alternating series successfully using Infinite Stream; however, I still need to display only between the 5th and 15th numbers.

--- P3Question6.fs
+++ P3Question6.fs
@@ -48,4 +48,33 @@ module P3Question6 =
         
 
     printfn "\nP3Question6B: %A" (specificElements)   
+    
+    
+    /------------------------------------------------------------
+    
+    
+    type 'a stream = 
+        Cons of 'a * (unit -> 'a stream)
+
+
+    let rec two = function
+        |0.0 -> 1.0
+        |powerNtimes -> 2.0 * two (powerNtimes-1.0)
+
+
+    let rec upfrom powerNtimes = Cons( 1.0 / (two powerNtimes) * (if ( (powerNtimes + 1.0) % 2.0 = 0.0) then 1.0 else -1.0), fun () -> upfrom(powerNtimes+1.0))
+
+
+    let altSeriesC = upfrom 1.0
+
+    
+    let rec take n (Cons(x,xsf)) =
+          if n = 0 then []
+                   else x :: take (n-1) ( xsf () )
+
+
+
+
+    printfn "\nP3Question6C: %A" (take 15 altSeriesC)
+
 
