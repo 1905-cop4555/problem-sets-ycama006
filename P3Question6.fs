432de763485ad6eed8c5e2d3964f2e701272c8ed
ycama006 <32624262+ycama006@users.noreply.github.com>
2019-06-10 23:36:50 -0400

    Problem Set 3, Question 6
    Displayed the 5th through 15th numbers in the series as the floating point version of the fractions.

--- P3Question6.fs
+++ P3Question6.fs
@@ -7,7 +7,11 @@ OBJECTIVE: (Problem Set 3, Question 6)
 
 module P3Question6 = 
 
-    let altSeries = Seq.initInfinite (fun index ->
+
+    // Generate an infinite sequence for the alternating series of 1/(2**n):
+    // 1/2, -1/4, 1/8, -1/16, 1/32, -1/64, ...
+
+    let altSeriesA = Seq.initInfinite (fun index ->
         let powerNtimes = float (index + 1)
         let rec two = function
             |0.0 -> 1.0
@@ -17,4 +21,31 @@ module P3Question6 =
         )
 
 
-    printfn "\n\nP3Question6: %A" (altSeries)
+    printfn "\n\nP3Question6A: %A" (altSeriesA)
+    
+    
+    
+    
+    //------------------------------------------------------------
+    
+    
+    // Display the 5th through 15th numbers in the series. 
+    // The numbers should display as the floating point version of the fractions.
+    
+    let altSeriesB = Seq.initInfinite (fun index ->
+        let powerNtimes = float (index + 5)
+        let rec two = function
+            |0.0 -> 1.0
+            |powerNtimes -> 2.0 * two (powerNtimes-1.0)
+
+        1.0 / (two powerNtimes) * (if ( (index + 2) % 2 = 0) then 1.0 else -1.0)
+        )
+
+    let specificElements = 
+        altSeriesB 
+        |> Seq.take 11
+        |> Seq.toList
+        
+
+    printfn "\nP3Question6B: %A" (specificElements)   
+
